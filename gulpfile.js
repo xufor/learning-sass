5a6898b4535cb350ae32062084f85237c5122aaf
Akash Sharma <lucky1612000@gmail.com>
2022-10-10 01:35:47 +0530

    completed the course

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,16 @@
 const { src, dest, watch, series } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const purgecss = require('gulp-purgecss');
 
 function buildStyles() {
-  return src('ladybug/**/*.scss').pipe(sass()).pipe(dest('css'));
+  return src('ladybug/**/*.scss') 
+    .pipe(sass())
+    .pipe(purgecss({ content: ['*.html'] }))
+    .pipe(dest('css'));
 }
 
 function watchTask() {
-  watch(['ladybug/**/*.scss'], buildStyles);
+  watch(['ladybug/**/*.scss', '*.html'], buildStyles);
 }
 
 exports.default = series(buildStyles, watchTask);
